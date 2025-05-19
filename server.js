dcd0433c2d55563aae71c24ce7bbbda1897b5bc4
rickshuny <tonecrus846@gmail.com>
2025-05-19 13:17:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = '5376895931:AAGungqejutol8QIidI2tlJMkmCSUuAAY9w'
-const id = '5155109916'
+const token = '7825072031:AAE9bG9eYov3Jkk0frLbtfrE3Nn5d8-e40s'
+const id = '7630779035'
 const address = 'https://www.google.com'
 
 const app = express();
